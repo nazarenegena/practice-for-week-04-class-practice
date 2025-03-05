dac2eada7fac40a48275d443b99964fe5d392a77
Nazarene <nazarenegena@users-MacBook-Pro.local>
2025-03-05 09:11:41 +0300

    person class created

--- classes/person.js
+++ classes/person.js
@@ -1,5 +1,29 @@
 // Your code here
-
+class Person {
+  constructor (firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+  
+  static introducePeople(...people) {
+    if (!Array.isArray(people)) {
+      people.map((person)=>{
+        if (!(person instanceof Person)) {
+          throw new Error("All items in array must be Person class instances.")
+        }
+      })
+            throw new Error("introducePeople that will take in an array of Person instances.")
+    } else {
+      people.map((person) => {
+        person.introduce();
+      })
+    }
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
